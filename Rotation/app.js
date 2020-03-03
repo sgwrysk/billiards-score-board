09913a20257aa362d9059b3537b4b852c464d4a8
Ryosuke Sogawa <latte.e.miele@gmail.com>
2020-03-03 15:14:19 +0900

    add functions.

--- Rotation/app.js
+++ Rotation/app.js
@@ -1,29 +1,114 @@
-const app = new Vue({
-  el: '#p1',
-  data: {
-    score: 0
-  },
-  methods: {
-    changeScore: function (v) {
-      this.score += v;
-    },
-    clear: function () {
-      this.score = 0;
-    }
-  }
-})
+var PointBtn = {
+  template: 
+    `<button 
+      class="pointBtnClass"
+      @click="$emit('change', _self)"
+      v-bind:class="status"
+      v-text="ball.point">
+    </button>`,
+  props: ['ball', 'pId'],
+  computed: {
+    status: function(){
+      if(this.ball.pId == -1){
+        return 'yet';
+      }
+      if(this.ball.pId == this.pId){
+        return 'get';
+      }else{
+        return 'lose';
+      }
 
-const app2 = new Vue({
-  el: '#p2',
-  data: {
-    score: 0
-  },
-  methods: {
-    changeScore: function (v) {
-      this.score += v;
-    },
-    clear: function () {
-      this.score = 0;
     }
   }
-})
+};
+
+
+var Player = {
+  template:
+    `<div v-bind:class="pClass">
+      {{pName}}<br>
+      <p class="scoreClass">
+        Score : {{ score }}
+      </p>
+      <point-btn
+        v-for="ball in Balls"
+        :key="ball.point"
+        :ball="ball"
+        :pId="pId"
+        @change="$emit('change',$event, _self)"/>
+    </div>`,
+  components: {
+    PointBtn
+  },
+  props: ['pName', 'pClass', 'Balls', 'pId'],
+  data: function(){
+    return {
+      score: 0
+    }
+  }
+};
+
+
+var poolTable = new Vue({
+  el: '#poolTable',
+  template:
+    `<div>
+      <button @click="nextRack">Next Rack</button>
+      <Player1 pId="1" pName="Player 1" pClass="p1" :Balls="Balls" @change="change"/>
+      <Player2 pId="2" pName="Player 2" pClass="p2" :Balls="Balls" @change="change"/>
+    </div>`,
+  components: {
+    Player1 : Player,
+    Player2 : Player
+  },
+  data: function(){
+    return {
+      Balls:[
+        { point: 1,  pId: -1},
+        { point: 2,  pId: -1},
+        { point: 3,  pId: -1},
+        { point: 4,  pId: -1},
+        { point: 5,  pId: -1},
+        { point: 6,  pId: -1},
+        { point: 7,  pId: -1},
+        { point: 8,  pId: -1},
+        { point: 9,  pId: -1},
+        { point: 10, pId: -1},
+        { point: 11, pId: -1},
+        { point: 12, pId: -1},
+        { point: 13, pId: -1},
+        { point: 14, pId: -1},
+        { point: 15, pId: -1}
+      ]
+    }
+  },
+  methods:{
+    change(btn, player){
+      if(btn.ball.pId == -1){
+          player.score = Number(player.score) + Number(btn.ball.point);
+          btn.ball.pId = player.pId;
+          return;
+      }
+
+      if(btn.ball.pId == player.pId){
+          player.score = Number(player.score) - Number(btn.ball.point);
+          btn.ball.pId = -1;
+          return;
+      }
+    },
+    reset(){
+      for(let b of this.Balls){
+        b.pId = -1;
+      }
+    },
+    nextRack(){
+      for(let b of this.Balls){
+        if(b.pId == -1) return;
+      }
+      for(let b of this.Balls){
+        b.pId = -1;
+      }
+    }
+  }
+});
+
